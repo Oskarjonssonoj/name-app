9f1b9891c92f49ee1a5922ad98e3776fabe3e466
Oskar Jönsson <peroskarjonsson@gmail.com>
2020-01-14 14:22:25 +0100

    added alternative way to fetch API

--- apps/name-app/javascript/script.js
+++ apps/name-app/javascript/script.js
@@ -1,6 +1,36 @@
-const searchBtn = document.querySelector("#search");
+const formSearch = document.querySelector("#formSearch");
+
 const nameEl = document.querySelector("#name-search");
+
 const countryEl = document.querySelector("#country-search");
+
+const content = document.querySelector("#content");
+
+// formSearch.addEventListener('sumbit', e => {
+//     e.preventDefault();
+
+//     const name = nameEl.value;
+//     const country = countryEl.value;
+
+//     const urlSearch = `https://api.abalin.net/getdate?name=${name}&country=${country}`;
+
+//     const getNames = async () => {
+//         const response = await fetch(urlSearch);
+
+//         const searchResult = await response.json();
+
+//         searchResult.results.forEach((data) => {
+//             output += `<p>${data.name}</p>
+//             <p>${data.month}/${data.day}</p>`;
+
+//             content.innerHTML = output;  
+//         });
+//     };
+//     getNames()  
+//     .catch(err => {
+//         console.error("Something went very wrong! Error was:", err);
+//     });
+// });
  
 searchBtn.addEventListener('click', async function() {
  
@@ -9,16 +39,14 @@ searchBtn.addEventListener('click', async function() {
  
     const response = await fetch(`https://api.abalin.net/getdate?name=${name}&country=${country}`);
     
-    const parse = await response.json()
+    const parse = await response.json();
  
     const content = document.querySelector("#content");
     let output = ""
  
-    parse.results.forEach((info) => {
-        output += `<p>${info.name}</p>
-        <p>${info.month}/${info.day}</p>`;
+    parse.results.forEach((data) => {
+        output += `<p>${data.name}</p>
+        <p>${data.month}/${data.day}</p>`;
      });
-
-    content.innerHTML = output;
-    
+    content.innerHTML = output;  
 });
